88c9a433290887ad7334d456793b3904c4e047c3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-06 16:58:07 +0000

    Add src/analytics/interface.py

---
+++ src/analytics/interface.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+import transformers
+
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import config
+import src.analytics.architectures
+
+
+class Interface:
+
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+        """
+
+        :param s3_parameters:
+        """
+
+        self.__service = service
+        self.__s3_parameters = s3_parameters
+
+        self.__configurations = config.Config()
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)30d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self):
+        """
+        os.path.join(configurations.artefacts_, 'distil', 'prime', 'model')
+
+        :return:
+        """
+
+        src.analytics.architectures.Architectures(
+            service=self.__service, s3_parameters=self.__s3_parameters).exc()
+
+
