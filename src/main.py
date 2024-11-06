413c51e7c7a3ba7be6075145db95a7b2b7f5b1a4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-06 17:00:12 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import sys
 import torch
 
 
-
 def main():
     """
     Entry Point
@@ -27,7 +26,7 @@ def main():
 
     # Analytics
     src.analytics.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
-    
+
     # Delete Cache Points
     src.functions.cache.Cache().exc()
 
