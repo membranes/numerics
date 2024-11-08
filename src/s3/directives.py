5faf8e5261393ab8452ae4f18f2db03de6e05685
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-08 21:19:06 +0000

    Add src/s3/directives.py: Under Development

---
+++ src/s3/directives.py
@@ -0,0 +1,21 @@
+
+import dask
+
+
+class Directives:
+
+    def __init__(self):
+
+        pass
+
+    def __get(self, path: str):
+
+        pass
+
+    def exc(self, paths):
+
+
+        computation = []
+        for path in paths:
+
+            self.__get(path=path)
