88149884006f74e53c8b45186bc13c867bb9b01c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-09 00:16:41 +0000

    Update src/s3/directives.py

--- src/s3/directives.py
+++ src/s3/directives.py
@@ -1,41 +1,62 @@
+"""Module directives.py"""
 import os
-import dask
+import subprocess
 import sys
 
+import dask
 import pandas as pd
-import subprocess
 
 import src.elements.s3_parameters as s3p
 import src.functions.directories
 
 
 class Directives:
+    """
+    Class Directives
+    """
 
     def __init__(self, s3_parameters: s3p.S3Parameters):
+        """
+
+        :param s3_parameters:
+        """
 
         self.__s3_parameters = s3_parameters
 
+        # Directories instances
         self.__directories = src.functions.directories.Directories()
 
     @dask.delayed
-    def __unload(self, src: str, dst: str) -> int:
+    def __unload(self, origin: str, target: str) -> int:
+        """
 
-        self.__directories.create(path=dst)
-        target = dst.replace(os.getcwd() + os.path.sep, '')
+        :param origin:
+        :param target:
+        :return:
+        """
 
-        path = f"s3://{self.__s3_parameters.internal}/{src}/"
-        state = subprocess.run(f"aws s3 cp {path} {target}/ --recursive", shell=True, check=True)
+        # Create the destination directory
+        self.__directories.create(path=target)
+
+        # Hence
+        destination = target.replace(os.getcwd() + os.path.sep, '')
+        source = f"s3://{self.__s3_parameters.internal}/{origin}/"
+        state = subprocess.run(f"aws s3 cp {source} {destination}/ --recursive", shell=True, check=True)
 
         return state.returncode
 
     def exc(self, source: pd.Series, destination: pd.Series):
+        """
 
+        :param source:
+        :param destination:
+        :return:
+        """
 
         computation = []
-        for src, dst in zip(source, destination):
-            state = self.__unload(src=src, dst=dst)
+        for origin, target in zip(source, destination):
+            state = self.__unload(origin=origin, target=target)
             computation.append(state)
-
         executions: list[int] = dask.compute(computation, scheduler='threads')[0]
 
         if all(executions) == 0:
