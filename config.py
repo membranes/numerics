b9b5ccfcfd454a2c800dfa9b89510de75801cbcb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-21 15:01:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -14,13 +14,10 @@ class Config:
 
         Variables denoting a path - including or excluding a filename - have an underscore suffix; this suffix is
         excluded for names such as warehouse, storage, depository, etc.<br><br>
-
-        The variable <b>self.s3_parameters_template_</b> points to a template of
-        Amazon S3 (Simple Storage Service) parameters & arguments.
         """
 
         # Template
-        self.s3_parameters_template_ = 'https://raw.githubusercontent.com/membranes/configurations/refs/heads/master/data/s3_parameters.yaml'
+        self.s3_parameters_key = 's3_parameters.yaml'
 
         # Temporary storage area for the artefacts
         self.data_: str = os.path.join(os.getcwd(), 'data')
@@ -30,6 +27,6 @@ class Config:
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.numerics_ = os.path.join(self.warehouse, 'numerics')
 
-        # Each architecture's prime model artefacts are within the {architecture}/prime/model path
+        # Each architecture's prime artefacts are within the {architecture}/prime/ path
         self.architectures = ['bert', 'distil', 'roberta', 'electra']
-        self.prime_model_anchor = '/prime/model'
+        self.prime_ = '/prime/'
