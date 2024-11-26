a91131a21d25c5b89030906c34644aca6d8e4c83
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-26 23:13:08 +0000

    Update src/analytics/interface.py

--- src/analytics/interface.py
+++ src/analytics/interface.py
@@ -1,7 +1,16 @@
+import logging
+
+import src.analytics.architecture
 
 
 class Interface:
 
     def __init__(self):
         pass
+
+    def exc(self):
+
+        message = src.analytics.architecture.Architecture().exc()
+
+        logging.info(message)
 
