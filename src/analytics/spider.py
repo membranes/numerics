b6aad33b37cbe79da8560b27aa68019c1bc9f3be
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-27 20:48:22 +0000

    Update src/analytics/spider.py

--- src/analytics/spider.py
+++ src/analytics/spider.py
@@ -8,14 +8,25 @@ class Spider:
     def __init__(self):
 
         self.__names = {'precision': "Precision", 'sensitivity': "Sensitivity", 'specificity': 'Specificity',
-                 'fscore': 'F Score', 'youden': "Youden's JJ Statistic", 'balanced_accuracy': 'Balanced Accuracy',
+                 'fscore': 'F Score', 'youden': "Youden's J Statistic", 'balanced_accuracy': 'Balanced Accuracy',
                  'standard_accuracy': 'Standard Accuracy'}
 
-    def exc(self, derivations: pd.DataFrame):
+    def exc(self, blob: pd.DataFrame):
+
+        derivations = blob.copy()
 
         categories = derivations['category'].unique()
 
+        # The tag & category values are required for data structuring
+        derivations.set_index(keys=['tag', 'category'], drop=False, inplace=True)
+
+        # Hence
         for category in categories:
 
-            excerpt = derivations.loc[derivations['category'] == category, self.__names.keys()]
+            excerpt: pd.DataFrame = derivations.loc[derivations['category'] == category, self.__names.keys()]
             logging.info(excerpt)
+
+            excerpt.rename(columns=self.__names, inplace=True)
+
+            dictionary = excerpt.to_dict(orient='tight')
+            logging.info(dictionary)
