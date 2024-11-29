9700ddc9e8e7eca95e6b80210d8fcadb04e42cc4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-29 21:29:06 +0000

    Update src/analytics/interface.py

--- src/analytics/interface.py
+++ src/analytics/interface.py
@@ -1,9 +1,13 @@
 """Module interface.py"""
 import logging
+import os
+
+import pandas as pd
 
 import config
 import src.analytics.architecture
-import src.analytics.estimates
+import src.analytics.derivations
+import src.analytics.spider
 
 
 class Interface:
@@ -12,13 +16,56 @@ class Interface:
 
         self.__configurations = config.Config()
 
+        # The architecture name of the best model, ...
+        self.__architecture: str = src.analytics.architecture.Architecture().exc()
+        logging.info('THE ARCHITECTURE: %s', self.__architecture)
+
+
+    def __cases(self):
+        """
+
+        :return: Each instance represents a distinct tag; tag = annotation &#x29FA; category.
+                 The frame must include the error matrix frequencies is tp, tn, fp, & fn.
+        """
+
+        path = os.path.join(self.__configurations.artefacts_, self.__architecture, self.__configurations.branch)
+
+        try:
+            cases = pd.read_json(path_or_buf=path, orient='index')
+        except ImportError as err:
+            raise err from err
+
+        return cases
+
+    @staticmethod
+    def __derivations(cases: pd.DataFrame) -> pd.DataFrame:
+        """
+        Appends a series of metrics to each instance.
+
+        :param cases: Each instance represents a distinct tag; tag = annotation &#x29FA; category.
+                      The frame must include the error matrix frequencies is tp, tn, fp, & fn.
+        :return:
+        """
+
+        derivations = src.analytics.derivations.Derivations(cases=cases).exc()
+        derivations.reset_index(drop=False, inplace=True)
+
+        derivations.rename(columns={'index': 'tag'}, inplace=True)
+
+        return derivations
+
     def exc(self):
         """
 
         :return:
         """
 
-        architecture = src.analytics.architecture.Architecture().exc()
-        logging.info(architecture)
+        cases = self.__cases()
+        derivations = self.__derivations(cases=cases)
 
-        src.analytics.estimates.Estimates(architecture=architecture).exc()
+        # Add a category column
+        derivations = derivations.assign(category=derivations['tag'].map(self.__configurations.categories))
+        logging.info(derivations)
+
+        # Spiders
+        src.analytics.spider.Spider().exc(blob=derivations)
