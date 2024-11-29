6ea858da3ed371b370a7106e865e9077668859c7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-29 21:50:14 +0000

    Update src/analytics/bullet.py

--- src/analytics/bullet.py
+++ src/analytics/bullet.py
@@ -1,5 +1,70 @@
+import logging
+import os
+
+import pandas as pd
+
+import config
+import src.functions.directories
+import src.functions.objects
+
 
 class Bullet:
 
     def __init__(self):
-        pass
+
+        self.__configurations = config.Config()
+
+        # The directory wherein the data files, for the spider graphs, are stored.
+        self.__path = os.path.join(self.__configurations.card_, 'bullet')
+        src.functions.directories.Directories().create(path=self.__path)
+
+        # An instance for reading & writing JSON (JavaScript Object Notation) files.
+        self.__objects = src.functions.objects.Objects()
+
+        # The metrics in focus.
+        self.__names = {'fnr': 'False Negative Rate', 'fpr': 'False Positive Rate'}
+
+    def __save(self, nodes: dict, name: str):
+        """
+
+        :param nodes: The dictionary of values for the spider graph
+        :param name: The name of the file; filename & extension.
+        :return:
+        """
+
+        message = self.__objects.write(nodes=nodes, path=os.path.join(self.__path, name))
+
+        return message
+
+    def exc(self, blob: pd.DataFrame):
+        """
+
+        :param blob:
+        :return:
+        """
+
+        derivations = blob.copy()
+
+        # The unique tag categories
+        categories = derivations['category'].unique()
+
+        # The tag & category values are required for data structuring
+        # derivations.set_index(keys=['tag', 'category'], drop=False, inplace=True)
+
+        # Hence
+        for category in categories:
+
+            name = self.__configurations.definition[category]
+            logging.info(name)
+
+            # The instances of the category
+            excerpt: pd.DataFrame = derivations.loc[derivations['category'] == category, self.__names.keys()]
+            excerpt.rename(columns=self.__names, inplace=True)
+
+            # The dictionary of the instances
+            nodes = excerpt.to_dict(orient='split')
+            logging.info(nodes)
+
+            # Save
+            # message = self.__save(nodes=nodes, name=f'{name}.json')
+            # logging.info(message)
