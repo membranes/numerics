e8e76043bee2bde47b127569f5123f8f06984aff
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-30 01:31:54 +0000

    Update graphs/assets/js/bullet.js

---
+++ graphs/assets/js/bullet.js
@@ -0,0 +1,60 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+
+var url = '../assets/menu/bullet.json'
+
+
+// Menu details
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.desc).text(entry.name));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
+
+});
+
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+
+function generateChart(fileNameKey) {
+
+    $.getJSON('../../../warehouse/card/bullet/' + fileNameKey + '.json', function (calculations) {
+
+
+
+
+    }).fail(function () {
+        console.log("Missing");
+        $('#container').empty();
+    });
+
+}
+
+
+
+
+
+
+
