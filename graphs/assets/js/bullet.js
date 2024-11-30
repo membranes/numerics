e6eb84a9a155a4de2fcef2f7bbfc441d77e76340
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-30 17:15:05 +0000

    Update graphs/assets/js/bullet.js

--- graphs/assets/js/bullet.js
+++ graphs/assets/js/bullet.js
@@ -1,3 +1,5 @@
+// noinspection DuplicatedCode
+
 var Highcharts;
 var optionSelected;
 var dropdown = $('#option_selector');
@@ -22,7 +24,6 @@ $.getJSON(url, function (data) {
 });
 
 
-
 // Dropdown
 dropdown.on('change', function (e) {
 
@@ -37,17 +38,18 @@ dropdown.on('change', function (e) {
 });
 
 
-
+// Graphs/s
 function generateChart(fileNameKey) {
 
-    $.getJSON('../../../warehouse/card/bullet/' + fileNameKey + '.json', function (calculations) {
+    $.getJSON('../../warehouse/card/bullet/' + fileNameKey + '.json', function (calculations) {
+
 
         // Indices
-        const iNegative = calculations.index.indexOf('False Negative Rate');
-        const iPositive = calculations.index.indexOf('False Positive Rate');
+        const iNegative = calculations.columns.indexOf('False Negative Rate');
+        const iPositive = calculations.columns.indexOf('False Positive Rate');
 
         // Categories
-        let categories = calculations.index;
+        const categories = calculations.index;
 
         // Variables
         var fnr = [],
@@ -58,19 +60,122 @@ function generateChart(fileNameKey) {
             fnr.push({
                 y: calculations.data[i][iNegative],
                 target: calculations.target[iNegative]
-            })
+            });
+
             fpr.push({
                 y: calculations.data[i][iPositive],
                 target: calculations.target[iPositive]
-            })
+            });
         }
 
 
 
+        Highcharts.setOptions({
+            chart: {
+                inverted: true,
+                marginLeft: 135,
+                type: 'bullet',
+                height: 125
+            },
+            title: {
+                text: null
+            },
+            legend: {
+                enabled: false
+            },
+            yAxis: {
+                gridLineWidth: 0
+            },
+            plotOptions: {
+                series: {
+                    pointPadding: 0.25,
+                    borderWidth: 0,
+                    color: '#000',
+                    targetOptions: {
+                        width: '200%',
+                        color: 'orange'
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            }
+        });
+
+
+        Highcharts.chart('container0001', {
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                plotBands: [{
+                    from: 0,
+                    to: 0.1,
+                    color: '#666'
+                }, {
+                    from: 0.1,
+                    to: 0.5,
+                    color: '#999'
+                }, {
+                    from: 0.5,
+                    to: 1,
+                    color: '#bbb'
+                }],
+                labels: {
+                    format: '{value}'
+                },
+                title: null,
+                min: 0,
+                max: 1
+            },
+            series: [{
+                data: fnr
+            }],
+            tooltip: {
+                pointFormat: '<b>{point.y}</b> (with expected maximum at {point.target})'
+            }
+        });
+
+
+        Highcharts.chart('container0002', {
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                plotBands: [{
+                    from: 0,
+                    to: 0.1,
+                    color: '#666'
+                }, {
+                    from: 0.1,
+                    to: 0.5,
+                    color: '#999'
+                }, {
+                    from: 0.5,
+                    to: 1,
+                    color: '#bbb'
+                }],
+                labels: {
+                    format: '{value}'
+                },
+                title: null,
+                min: 0,
+                max: 1
+            },
+            series: [{
+                data: fpr
+            }],
+            tooltip: {
+                pointFormat: '<b>{point.y}</b> (with expected maximum at {point.target})'
+            }
+        });
+
 
     }).fail(function () {
         console.log("Missing");
-        $('#container').empty();
     });
 
 }
