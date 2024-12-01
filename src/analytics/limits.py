a7573e6609e6bb792614bc891b35d1a6dfb41dd5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-01 17:56:05 +0000

    Update src/analytics/limits.py

--- src/analytics/limits.py
+++ src/analytics/limits.py
@@ -22,11 +22,14 @@ class Limits:
         self.__service: sr.Service = service
         self.__s3_parameters = s3_parameters
 
+        # Prefix: The segments between the bucket name and a target file name.
+        self.__prefix = 'limits/'
+
     def __get_dictionary(self, key_name: str) -> dict:
         """
-        s3:// {bucket.name} / {prefix} / {filename}
+        s3:// {bucket.name} / {prefix} & {filename}
 
-        :param key_name: {prefix} / {filename}
+        :param key_name: {prefix} & {filename}
         :return:
         """
 
@@ -36,15 +39,15 @@ class Limits:
 
         return dictionary
 
-    def exc(self, key_name: str) -> dict:
+    def exc(self, filename: str) -> dict:
         """
-        s3:// {bucket.name} / {prefix} / {filename}
+        s3:// {bucket.name} / {prefix} & {filename}
 
-        :param key_name: {prefix} / {filename}
+        :param filename: A file's name, including its extension.
         :return:
         """
 
         # Get the dictionary of ...
-        dictionary = self.__get_dictionary(key_name=key_name)
+        dictionary = self.__get_dictionary(key_name=f'{self.__prefix}{filename}')
 
         return dictionary
