db899c435c98bca66a3b79d2edd370bcd31878ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-04 22:46:06 +0000

    Update graphs/assets/js/cost.js

---
+++ graphs/assets/js/cost.js
@@ -0,0 +1,62 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+
+var url = '../assets/menu/cost.json'
+
+
+// Menu details
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.desc).text(entry.name));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+// Graphs/s
+function generateChart(fileNameKey) {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
