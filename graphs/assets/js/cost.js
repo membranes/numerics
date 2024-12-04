ec13c4ad3481853c494019e07c6306638e921c6c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-04 23:14:10 +0000

    Update graphs/assets/js/cost.js

--- graphs/assets/js/cost.js
+++ graphs/assets/js/cost.js
@@ -111,6 +111,67 @@ function generateChart(fileNameKey) {
 
     $.getJSON('../../warehouse/numerics/cost/fpr/' + fileNameKey + '.json', function (calculations) {
 
+        var data = calculations.data;
+
+        Highcharts.chart("container0002", {
+            chart: {
+                type: "arearange",
+                zooming: {
+                    type: "x",
+                },
+                scrollablePlotArea: {
+                    minWidth: 600,
+                    scrollPositionX: 1,
+                },
+            },
+            title: {
+                text: "Temperature variation by day",
+                align: "left",
+            },
+            subtitle: {
+                text:
+                    "Source: " +
+                    '<a href="github.com/membranes"' +
+                    'target="_blank">entity detection ...</a>',
+                align: "left",
+            },
+            xAxis: {
+                // type: "datetime"
+            },
+            yAxis: {
+                title: {
+                    text: null,
+                },
+            },
+            tooltip: {
+                crosshairs: true,
+                shared: true,
+                valueSuffix: "£"
+                // xDateFormat: "%A, %b %e"
+            },
+            legend: {
+                enabled: false,
+            },
+            series: [
+                {
+                    name: "Cost",
+                    color: {
+                        linearGradient: {
+                            x1: 0,
+                            x2: 0,
+                            y1: 0,
+                            y2: 1,
+                        },
+                        stops: [
+                            [0, "#ff0000"],
+                            [1, "#000000"],
+                        ],
+                    },
+                    data: data
+                }
+            ]
+        });
+
 
     }).fail(function () {
         console.log("Missing");
@@ -119,21 +180,3 @@ function generateChart(fileNameKey) {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
