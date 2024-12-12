998f7596162e7c8a0e29adfb6f3ca0f99398fb81
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-12 19:32:45 +0000

    Update src/analytics/cost.py

--- src/analytics/cost.py
+++ src/analytics/cost.py
@@ -7,11 +7,9 @@ import numpy as np
 import pandas as pd
 
 import config
-import src.analytics.limits
-import src.elements.s3_parameters as s3p
-import src.functions.objects
 import src.analytics.cost_false_negative_rate as cfn
 import src.analytics.cost_false_positive_rate as cfp
+import src.functions.objects
 
 
 class Cost:
@@ -19,28 +17,27 @@ class Cost:
     Class Costs
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self, costs: pd.DataFrame, frequencies: pd.DataFrame):
         """
 
-        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
-                              settings of this project, e.g., region code name, buckets, etc.
+        :param costs
+        :param frequencies
         """
 
-        self.__s3_parameters = s3_parameters
+        self.__costs = costs
+        self.__frequencies = frequencies
 
         # Configurations
         self.__configurations = config.Config()
         self.__objects = src.functions.objects.Objects()
 
-        # Limits instance
-        self.__limits = src.analytics.limits.Limits(s3_parameters=self.__s3_parameters)
-        self.__costs: pd.DataFrame = self.__limits.exc(filename='costs.json', orient='split')
-        self.__frequencies: pd.DataFrame = self.__limits.exc(filename='frequencies.json', orient='index')
-
-        # Rates
+        # Rates, self.__rates: np.ndarray = self.__rates[..., None]
         self.__rates: np.ndarray = np.linspace(start=0, stop=1, num=101)
-        self.__rates: np.ndarray = self.__rates[1:]
-        self.__rates: np.ndarray = self.__rates[..., None]
+        self.__rates: np.ndarray = (self.__rates[1:])[..., None]
+
+        # Instances
+        self.__cfn = cfn.CostFalseNegativeRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
+        self.__cfp = cfp.CostFalsePositiveRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
 
     @dask.delayed
     def __fnr(self, category: str) -> dict:
@@ -50,9 +47,9 @@ class Cost:
         :return:
         """
 
-        item = cfn.CostFalseNegativeRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
+        # item = cfn.CostFalseNegativeRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
 
-        return item.exc(category=category)
+        return self.__cfn.exc(category=category)
 
     @dask.delayed
     def __fpr(self, category: str) -> dict:
@@ -62,9 +59,9 @@ class Cost:
         :return:
         """
 
-        item = cfp.CostFalsePositiveRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
+        # item = cfp.CostFalsePositiveRate(rates=self.__rates, costs=self.__costs, frequencies=self.__frequencies)
 
-        return item.exc(category=category)
+        return self.__cfp.exc(category=category)
 
     @dask.delayed
     def __persist(self, nodes: dict, metric: str, category: str) -> str:
@@ -93,12 +90,11 @@ class Cost:
         for category in categories:
 
             fnr = self.__fnr(category=category)
-            message_fnr = self.__persist(nodes=fnr, metric='fnr', category=category)
-
+            _fnr = self.__persist(nodes=fnr, metric='fnr', category=category)
             fpr = self.__fpr(category=category)
-            message_fpr = self.__persist(nodes=fpr, metric='fpr', category=category)
+            _fpr = self.__persist(nodes=fpr, metric='fpr', category=category)
 
-            computations.append([message_fnr, message_fpr])
+            computations.append([_fnr, _fpr])
 
         calculations = dask.compute(computations, scheduler='threads')[0]
         logging.info(calculations)
