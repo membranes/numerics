a1dfff26a3c5e4dca24cabd6ff263a295cd2c0a5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-06 22:58:26 +0100

    Add src/abstracts/bars.py

---
+++ src/abstracts/bars.py
@@ -0,0 +1,59 @@
+import collections
+import glob
+import logging
+import os
+
+import pandas as pd
+
+import config
+import src.elements.text_attributes as txa
+import src.functions.streams
+
+
+class Bars:
+
+    def __init__(self, tags: pd.DataFrame):
+
+        self.__tags = tags
+
+        self.__configurations = config.Config()
+        self.__streams = src.functions.streams.Streams()
+
+    def __data(self, uri: str) -> pd.DataFrame:
+        """
+
+        :param uri:
+        :return:
+        """
+
+        text = txa.TextAttributes(uri=uri, header=0)
+
+        return self.__streams.read(text=text)
+
+    def __frequencies(self, data: pd.DataFrame):
+        """
+
+        :param data:
+        :return:
+        """
+
+        # Tags: tag/annotation/annotation_name/category/category_name
+        descriptions = self.__tags[['tag', 'group']].set_index('tag').to_dict()['group']
+        frequencies = data['tagstr'].str.upper().str.split(pat=',', n=-1, expand=False).map(collections.Counter).sum()
+        items = [[k, frequencies[k], descriptions[k]] for k, v in frequencies.items()]
+
+        # As a data frame
+        frame = pd.DataFrame(data=items, columns=['tag', 'frequency', 'group'])
+        frame = frame.copy().merge(self.__tags[['tag', 'annotation_name']], on='tag', how='left')
+
+        return frame
+
+
+    def exc(self, architecture: str):
+
+        uri_ = glob.glob(pathname=os.path.join(self.__configurations.artefacts_, architecture, 'data', '*.csv'))
+
+        for uri in uri_:
+            data = self.__data(uri=uri)
+            frequencies = self.__frequencies(data=data)
+            logging.info(frequencies)
